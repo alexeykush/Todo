ef26f81849e91957f851fd882a228b4775415a01
Alexey Kushnarov <alexey.kushnarov@ukr.net>
2019-05-22 21:15:25 +0300

    basic front structure

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+
+const initialState = {
+    isAuth: false,
+    user: {}
+};
+
+export default function (state = initialState, { type, payload }) {
+    switch (type) {
+        default:
+            return state
+    }
+}
