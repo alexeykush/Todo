22e1b31fb75a65e9e3ce89a1839669524e8c8bda
Alexey Kushnarov <alexey.kushnarov@ukr.net>
2019-05-27 01:21:56 +0300

    h e r o k u

--- server/app.js
+++ server/app.js
@@ -21,12 +21,6 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 });
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-app.use(express.static(__dirname + '/client/build'));
-app.use(express.static(path.join(__dirname, "client")));
-app.use(express.static(path.join(__dirname, "client", 'build')));
-app.use(express.static("client/build"));
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -44,10 +38,13 @@ app.use('/api/lists', listsRoute);
 app.use("/api/images", imagesRoute);
 
 
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
