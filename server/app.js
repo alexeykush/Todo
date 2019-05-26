ead0f0d7827848a3660a6b2fa71de549034d6d11
Alexey Kushnarov <alexey.kushnarov@ukr.net>
2019-05-27 00:04:58 +0300

    try fixes white screen on iphone

--- server/app.js
+++ server/app.js
@@ -25,6 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use(express.static("client/build"));
 app.use(passport.initialize());
 require("./config/passport")(passport);
 
@@ -38,7 +39,6 @@ app.use('/api/notes', notesRoute);
 app.use('/api/lists', listsRoute);
 app.use("/api/images", imagesRoute);
 
-app.use(express.static("client/build"));
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
