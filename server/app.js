3cc8b2a8214c2716e12ff16c86f5616d54456953
Alexey Kushnarov <alexey.kushnarov@ukr.net>
2019-05-26 23:40:22 +0300

    try fixes white screen on iphone

--- server/app.js
+++ server/app.js
@@ -33,18 +33,19 @@ mongoose
     .then(() => console.log("Mongo connected"))
     .catch(e => console.log(e));
 
-app.use('/api/users', usersRoute);
-app.use('/api/notes', notesRoute);
-app.use('/api/lists', listsRoute);
-app.use("/api/images", imagesRoute);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, 'build')));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     })
 }
+
+app.use('/api/users', usersRoute);
+app.use('/api/notes', notesRoute);
+app.use('/api/lists', listsRoute);
+app.use("/api/images", imagesRoute);
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
