a86a4465e9854d478736c3b42a354d0b18baa176
Alexey Kushnarov <alexey.kushnarov@ukr.net>
2019-05-27 01:25:56 +0300

    h e r o k u

--- server/app.js
+++ server/app.js
@@ -40,9 +40,10 @@ app.use("/api/images", imagesRoute);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendfile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
 app.listen(port, () => {
