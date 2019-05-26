b88ad91b6390ba648ce662036b3c4637a5bf752a
Alexey Kushnarov <alexey.kushnarov@ukr.net>
2019-05-27 00:45:43 +0300

    fixes for heroku

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ cloudinary.config({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client/build"));
 app.use(passport.initialize());
 require("./config/passport")(passport);
 
@@ -40,7 +40,7 @@ app.use("/api/images", imagesRoute);
 
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"))
 });
 
 app.listen(port, () => {
