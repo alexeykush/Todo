96c7f1e6a2dce6cf8a61f2dd12aafa356913a567
Alexey Kushnarov <alexey.kushnarov@ukr.net>
2019-05-27 00:12:19 +0300

    fixes heroku

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname,"client", "build")));
 app.use(passport.initialize());
 require("./config/passport")(passport);
 
