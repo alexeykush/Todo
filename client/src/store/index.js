4ba906017d055910dfb662d74a9807450e85b319
Alexey Kushnarov <alexey.kushnarov@ukr.net>
2019-05-27 11:43:26 +0300

    redux fixes

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "../reducers";
@@ -6,13 +6,25 @@ import rootReducer from "../reducers";
 const initialState = {};
 const middlewares = [thunk];
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middlewares),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+let store = null;
+
+if (window.navigator.userAgent.includes("Chrome") && process.env.NODE_ENV === "development") {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middlewares),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    );
+} else {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middlewares)
+        )
+    );
+}
 
 export default store;
--- server/app.js
+++ server/app.js
@@ -45,7 +45,6 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
